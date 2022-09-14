c0c72a7ddc37edd768920d402c4d148a32999329
Szymon Poltorak <szymonpotorak@gmail.com>
2022-09-14 16:44:02 +0200

    Basic ui for game scene

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-<a href="https://www.flaticon.com/free-icons/chess" title="chess icons">
-    Chess icons created by Freepik - Flaticon
-</a>
+[Chess Icon](https://www.flaticon.com/free-icon/horse_3637390?term=chess&page=1&position=35&page=1&position=35&related_id=3637390&origin=tag)\
+[User Icon](https://www.flaticon.com/free-icon/programmer_560277)\
+[Play Icon](https://www.svgrepo.com/svg/169150/chess)\
+[Analysis Icon](https://www.svgrepo.com/svg/137390/analysis)\
+[Players Icon](https://www.svgrepo.com/svg/85514/people-hand-drawn-persons-group)\
+[Profile Icon](https://www.svgrepo.com/svg/315765/profile)\
+[Engine Icon](https://www.svgrepo.com/svg/3999/computer)\
+[Settings Icon](https://www.svgrepo.com/svg/13646/settings)
--- src/main/GameWindow.py
+++ src/main/GameWindow.py
@@ -1,7 +1,9 @@
+import ctypes
+
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import QWidget
 
-from src.main.GameWindowUi import GameWindowUi
+from src.main.GameWindowUi_test import GameWindowUi
 
 
 class GameWindow(QWidget):
@@ -11,9 +13,12 @@ class GameWindow(QWidget):
         super(GameWindow, self).__init__()
         width = canvas.get_width()
         height = canvas.get_height()
+        appid = "mycompany.myproduct.subproduct.version"
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(appid)
 
         with open("src/resources/styles/GameWindow.min.css", "r", encoding="utf-8") as style:
-            self.__ui = GameWindowUi(self)
+            self.__ui = GameWindowUi()
+            self.__ui.setup_ui(self)
             self.__data = canvas.get_buffer().raw
             self.__image = QtGui.QImage(self.__data, width, height, QtGui.QImage.Format_RGB32)
             self.setStyleSheet(style.read())
@@ -21,7 +26,7 @@ class GameWindow(QWidget):
     def paintEvent(self, event):
         canvas_painter = QtGui.QPainter()
         canvas_painter.begin(self)
-        canvas_painter.drawImage(110, 80, self.__image)
+        canvas_painter.drawImage(150, 80, self.__image)
         canvas_painter.end()
 
     def get_ui(self):
